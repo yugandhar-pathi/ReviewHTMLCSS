25a27d013354defcda6aa5468f7703e7c410f422
Kalyani <kalyanichow8@gmail.com>
2022-04-06 21:04:16 -0700

    fixed review comments

--- Student/student.js
+++ Student/student.js
@@ -1,9 +1,9 @@
 let studentArray;
 let studentIdToDelete;
-let dialog = document.getElementById('dialogId')
+let dialog = document.getElementById('deleteWarningDlg')
 let errDialog = document.getElementById("errorDialog")
-let studentDetailsPopup = document.getElementById("displayStudentDetailsId")
-let addDialog = document.getElementById("addStuDia")
+let studentDetailsPopup = document.getElementById("displayStudentDetailsDlg")
+let addDialog = document.getElementById("addStudentDlg")
 let deleteStudent = (event) => {
     studentIdToDelete = event.target.id;
     dialog.showModal()
@@ -44,6 +44,24 @@ let closeDialogStudentDEtails = () => {
     studentDetailsPopup.close()
 }
 
+let createListItem = (name, grade, id) => {
+    let studentListDiv = document.createElement('div')
+    let studentListItem = document.createElement('li')
+    studentListItem.id = `student${id}`
+    let textNode = document.createTextNode(`Student Name : ${name} Student grade: ${grade}`)
+    studentListItem.appendChild(textNode)
+    studentListItem.onclick = popupStudent
+    studentListDiv.appendChild(studentListItem)
+
+    let deleteButton = document.createElement('button')
+    deleteButton.id = `${id}delete`
+    deleteButton.innerText = "Delete"
+    deleteButton.onclick = deleteStudent
+    studentListDiv.appendChild(deleteButton);
+
+    return studentListDiv
+
+}
 
 fetch("/api/students")
     .then(res => res.json())
@@ -51,20 +69,8 @@ fetch("/api/students")
         studentArray = res;
 
         studentArray.forEach(student => {
-            let studentListDiv = document.createElement('div')
-            let studentListItem = document.createElement('li')
-            studentListItem.id = `student${student.id}`
-            let textNode = document.createTextNode(`Student Name : ${student.name} Student grade: ${student.grade}`)
-            studentListItem.appendChild(textNode)
-            studentListItem.onclick = popupStudent
-            studentListDiv.appendChild(studentListItem)
-
-            let deleteButton = document.createElement('button')
-            deleteButton.id = `${student.id}delete`
-            deleteButton.innerText = "Delete"
-            deleteButton.onclick = deleteStudent
-            studentListDiv.appendChild(deleteButton);
-            document.getElementById('studentUlId').appendChild(studentListDiv)
+            let stuDiv = createListItem(student.name, student.grade, student.id)
+            document.getElementById('studentUlId').appendChild(stuDiv)
         });
     })
 
@@ -73,26 +79,32 @@ let addStudent = () => {
     addDialog.showModal()
 }
 
+
+
 let add = () => {
+    let name = document.getElementById("stuName").value
+    let grade = document.getElementById("stuGrade").value
+
+    let studentData = {
+        stuName: name,
+        stuGrade: grade
+    }
+
     fetch("/api/addstudent", {
-        method: "POST"
+        method: "POST",
+        body: JSON.stringify(studentData)
     })
         .then(res => res.json())
         .then(res => {
 
             if (res.status == "success") {
-                let listEle = document.createElement('li')
-                let name = document.getElementById("stuName").value
-                let grade = document.getElementById("stuGrade").value
-                listEle.innerHTML = `Student Name : ${name} Student Grade : ${grade}`
-                document.getElementById("studentUlId").appendChild(listEle)
+                let stuDiv = createListItem(studentData.stuName, studentData.stuGrade)
+                document.getElementById("studentUlId").appendChild(stuDiv)
 
             }
         })
     addDialog.close()
 }
-
-
 
 document.getElementById("addStudent").addEventListener('click', addStudent)
 
--- cypress/integration/test-student.js
+++ cypress/integration/test-student.js
@@ -3,19 +3,19 @@ it("Dispaly student details", () => {
     //delete list item
     cy.get("[id=studentUlId]").children().should("have.length", 6)
     cy.get("[id=1delete]").click()
-    cy.get("[id=dialogId]").should("be.visible")
+    cy.get("[id=deleteWarningDlg]").should("be.visible")
     cy.get("[id=removeStd]").click()
     cy.get("[id=studentUlId]").children().should("have.length", 5)
 
     //Display student information
     cy.get("[id=student2]").click()
-    cy.get("[id=displayStudentDetailsId]").should("be.visible").contains("Leia")
+    cy.get("[id=displayStudentDetailsDlg]").should("be.visible").contains("Leia")
     cy.get("[id=closeDialogStudentDetails]").click()
-    cy.get("[id=displayStudentDetailsId]").should("not.be.visible")
+    cy.get("[id=displayStudentDetailsDlg]").should("not.be.visible")
 
     //Add student
     cy.get("[id=addStudent]").click()
-    cy.get("[id=addStuDia]").should("be.visible")
+    cy.get("[id=addStudentDlg]").should("be.visible")
     cy.get("[id=stuName]").type("Kalyani")
     cy.get("[id=stuGrade]").type("4")
     cy.get("[id=add]").click()
